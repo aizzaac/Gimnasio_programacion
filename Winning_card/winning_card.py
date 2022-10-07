b32b2d6c79857423314d31a6ce421c8c3893e4b9
aizzaac <68517348+aizzaac@users.noreply.github.com>
2022-10-06 19:07:24 -0500

    Update winning_card.py

--- Winning_card/winning_card.py
+++ Winning_card/winning_card.py
@@ -1,6 +1,23 @@
+'''
+CARTA GANADORA
+En un juego de cartas, cada jugador recibe aleatoriamente una mano de cartas. Cuando los jugadores lancen sobre la mesa su única carta ganadora, 
+ganará el que posea la carta más alta. 
+Una carta ganadora es aquella que sólo existe una vez en la mano de cartas, y además es la más alta. 
 
+Dada una matriz de conjuntos de enteros de cartas, devuelva la carta del jugador ganador. Si tal carta no existe retornar -1.
 
+Ejemplo 1:
+- Entrada: cards = [[5,7,3,9,4,9,8,3,1], [1,2,2,4,4,1], [1,2,3]]
+- Salida: 8
 
+Ejemplo 2:
+- Entrada: cards = [[5, 5], [2, 2]]
+- Salida: -1
+
+Restricciones:
+- 1 <= cards.length <= 2000
+- 0 <= cards[i] <= 1000
+'''
 
 
 import pytest
