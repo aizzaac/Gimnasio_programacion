c45122c6a3d8d5b2293b78bbfa2b8bfc56294f72
aizzaac <68517348+aizzaac@users.noreply.github.com>
2022-10-28 17:38:23 -0500

    Update valid_parentheses.py

--- Valid_parentheses/valid_parentheses.py
+++ Valid_parentheses/valid_parentheses.py
@@ -33,23 +33,52 @@ Ejemplo 5:
 
 import pytest
 
-
 def valid_parentheses(s):
-  
+    stack = []
+
+    #Evaluar si el número de elementos es impar
+    if len(s) % 2 != 0:
+        return "invalid"
+    
+    #Iterar en el string, almacenar en la pila si el caracter es de apertura
+    for char in s:
+        if char in ["(", "[", "{"]:
+            stack.append(char)
+
+        #Evaluar si el caracter es de clausura, y si la pila está vacía
+        else:
+            if not stack:
+                return "invalid"
+    
+            char_stack = stack.pop()
+            if char_stack == "(" and char != ")":
+                return "invalid"
+
+            if char_stack == "[" and char != "]":
+                return "invalid"
+            
+            if char_stack == "{" and char != "}":
+                return "invalid"
+
+    #Evaluar si la pila está llena
+    if stack:
+        return "invalid"
+    return "valid"
+
+
 class TestClass:
   def test_one(self):
-    assert valid_parentheses("()") == valid, "La respuesta debe ser: válido"
+    assert valid_parentheses("()") == "valid", "La respuesta debe ser: válido"
   def test_two(self):
-    assert valid_parentheses("()[]{}") == valid, "La respuesta debe ser: válido"
+    assert valid_parentheses("()[]{}") == "valid", "La respuesta debe ser: válido"
   def test_three(self):
-    assert valid_parentheses("(]") == invalid, "La respuesta debe ser: inválido"
+    assert valid_parentheses("(]") == "invalid", "La respuesta debe ser: inválido"
   def test_four(self):
-    assert valid_parentheses("(])]") == invalid, "La respuesta debe ser: inválido"
+    assert valid_parentheses("([)]") == "invalid", "La respuesta debe ser: inválido"
   def test_five(self):
-    assert valid_parentheses("{[]}") == valid, "La respuesta debe ser: válido"
+    assert valid_parentheses("{[]}") == "valid", "La respuesta debe ser: válido"
 
 unit_test = TestClass()
-
 if __name__ == "__main__":
     pytest.main()
-
+    
